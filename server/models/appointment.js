e0beaf7697058474983786d135ee65b6596e446d
Alvin  Turner <a.turner3x@gmail.com>
2023-07-12 23:52:20 +0000

    created appointment model , imported to index.js

---
+++ server/models/appointment.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const appointmentSchema = new mongoose.Schema({
+  customerName: { type: String, required: true },
+  stylistName: { type: String, required: true },
+  appointmentDate: { type: Date, required: true },
+  appointmentTime: { type: String, required: true },
+});
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+
+module.exports = Appointment;
