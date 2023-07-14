8fb72d7be8847ef461f29729ca29d945f195bd47
Joshua Cavell <xclusive36@gmail.com>
2023-07-13 21:12:43 -0400

    setup client to use ion-page

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,35 @@
-import React from 'react';
+import React from "react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { IonPage, IonHeader, IonContent, IonFooter } from "@ionic/react";
 
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Profile from './pages/Profile';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -43,34 +44,23 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
+        <IonPage>
+          <IonHeader>
+            <Header />
+          </IonHeader>
+          <IonContent className="ion-padding">
             <Routes>
-              <Route 
-                path="/"
-                element={<Home />}
-              />
-              <Route 
-                path="/login"
-                element={<Login />}
-              />
-              <Route 
-                path="/signup"
-                element={<Signup />}
-              />
-              <Route 
-                path="/me"
-                element={<Profile />}
-              />
-              <Route 
-                path="/profiles/:username"
-                element={<Profile />}
-              />
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/me" element={<Profile />} />
+              <Route path="/profiles/:username" element={<Profile />} />
             </Routes>
-          </div>
-          <Footer />
-        </div>
+          </IonContent>
+          <IonFooter>
+            <Footer />
+          </IonFooter>
+        </IonPage>
       </Router>
     </ApolloProvider>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import {IonContent, IonDatetime} from '@ionic/react';
+import React from "react";
 
 const Home = () => {
   return (
-    <IonContent className="ion-padding">
-        <div className="book-here">
-          <h2>Book Your Appointment Here!</h2>
-        </div>
-        <div className="info-input">
-          <label for="info-input">Enter name and email:</label>
-          <input type="text" id="name" name="Name"/>
-          <input type="text" id="email" name="Email"/>
-        </div>
-        <div className="services">
-          <label for="services">Select a service:</label>
-          <button className="service-btn">Haircut</button>
-          <button className="service-btn">Color</button>
-          <button className="service-btn">Perm</button>
-          <button className="service-btn">Extensions</button>
-          <button className="service-btn">Style</button>
-        </div>
-        <IonDatetime value="2023-12-31T09:00" min="2023-07-13T09:00" max="2023-12-30T20:00">
-        </IonDatetime>
-        </IonContent>
-  )
+    <div>
+      <div className="book-here">
+        <h2>Book Your Appointment Here!</h2>
+      </div>
+      <div className="info-input">
+        <label>Enter name and email:</label>
+        <input type="text" id="name" name="Name" />
+        <input type="text" id="email" name="Email" />
+      </div>
+      <div className="services">
+        <label>Select a service:</label>
+        <button className="service-btn">Haircut</button>
+        <button className="service-btn">Color</button>
+        <button className="service-btn">Perm</button>
+        <button className="service-btn">Extensions</button>
+        <button className="service-btn">Style</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
