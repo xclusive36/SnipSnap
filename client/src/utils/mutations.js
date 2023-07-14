e3925786c4c740e9e5c9c10345e9642ccb8e92c5
NickLoeffler999 <123021014+NickLoeffler999@users.noreply.github.com>
2023-07-13 20:33:10 -0400

    updated mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,16 +1,4 @@
-import { gql } from '@apollo/client';
-
-export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
-`;
+import { gql } from "@apollo/client";
 
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
@@ -19,38 +7,64 @@ export const ADD_USER = gql`
       user {
         _id
         username
+        email
       }
     }
   }
 `;
 
-export const ADD_THOUGHT = gql`
-  mutation addThought($thoughtText: String!) {
-    addThought(thoughtText: $thoughtText) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
         _id
-        commentText
+        username
+        email
       }
     }
   }
 `;
 
-export const ADD_COMMENT = gql`
-  mutation addComment($thoughtId: ID!, $commentText: String!) {
-    addComment(thoughtId: $thoughtId, commentText: $commentText) {
+export const ADD_STYLIST = gql`
+  mutation addStylist($stylistName: String!) {
+    addStylist(stylistName: $stylistName) {
       _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        createdAt
-      }
+      stylistName
+    }
+  }
+`;
+
+export const ADD_APPOINTMENT = gql`
+  mutation addAppointment(
+    $customerName: String!
+    $stylistName: String!
+    $appointmentDate: String!
+    $appointmentTime: String!
+  ) {
+    addAppointment(
+      customerName: $customerName
+      stylistName: $stylistName
+      appointmentDate: $appointmentDate
+      appointmentTime: $appointmentTime
+    ) {
+      _id
+      customerName
+      stylistName
+      appointmentDate
+      appointmentTime
+    }
+  }
+`;
+
+export const REMOVE_APPOINTMENT = gql`
+  mutation removeAppointment($appointmentId: ID!) {
+    removeAppointment(appointmentId: $appointmentId) {
+      _id
+      customerName
+      stylistName
+      appointmentDate
+      appointmentTime
     }
   }
 `;
