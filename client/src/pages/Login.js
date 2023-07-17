973891690f32385186d97dd30eee320ccf39cdfe
Mary Dault <bluestar1dault@gmail.com>
2023-07-17 19:47:17 -0400

    import IonItem

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
-import { IonCard, IonInput, IonButton } from '@ionic/react';
+import { IonCard, IonInput, IonButton, IonItem } from '@ionic/react';
 
 import Auth from '../utils/auth';
 
