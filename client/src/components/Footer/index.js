fc272fd0330a57d21801ae1e472e66d0349e03b9
Mary Dault <bluestar1dault@gmail.com>
2023-07-19 18:48:42 -0400

    hours of operation

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,6 +4,10 @@ import { IonToolbar } from "@ionic/react";
 const Footer = () => {
   return (
     <footer>
+      <IonToolbar className="ion-text-center">Hours of Operation:</IonToolbar>
+      <IonToolbar className="ion-text-center">Monday - Friday: 9am - 7pm</IonToolbar>
+      <IonToolbar className="ion-text-center">Saturday: 9am - 6pm</IonToolbar>
+      <IonToolbar className="ion-text-center">Sunday: 10am - 5pm</IonToolbar>
       <IonToolbar className="ion-text-center">Copyright 2023 &#169; SnipSnap</IonToolbar>
     </footer>
   );
