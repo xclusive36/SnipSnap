a6d97aae7d7e66dd92ffc33dbe5b08b96665ec05
Amadeus <a.turner3x@gmail.com>
2023-07-20 19:44:35 -0400

    added comments to footer component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,14 +3,18 @@ import { IonToolbar } from "@ionic/react";
 
 const Footer = () => {
   return (
+    // Footer section containing contact and copyright information
     <footer>
-        <IonToolbar color="primary" className="ion-text-center">
-          <p>Hours of Operation:</p>
-          <p>Monday - Friday: 9am - 7pm</p>
-          <p>Saturday: 9am - 6pm</p>
-          <p>Sunday: 10am - 5pm</p>
-          <p>Copyright 2023 &#169; SnipSnap</p>
-        </IonToolbar>
+      {/* IonToolbar for consistent styling and alignment */}
+      <IonToolbar color="primary" className="ion-text-center">
+        {/* Operating hours */}
+        <p>Hours of Operation:</p>
+        <p>Monday - Friday: 9am - 7pm</p>
+        <p>Saturday: 9am - 6pm</p>
+        <p>Sunday: 10am - 5pm</p>
+        {/* Copyright notice */}
+        <p>Copyright 2023 &#169; SnipSnap</p>
+      </IonToolbar>
     </footer>
   );
 };
